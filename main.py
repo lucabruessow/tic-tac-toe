1e69ab926474d5132f659a86b4dd21da7a5af578
Luca Brüssow <210828907+lucabruessow@users.noreply.github.com>
2025-06-05 12:04:48 +0200

    fix: add check for already taken cell

--- main.py
+++ main.py
@@ -60,31 +60,44 @@ class Player:
             if inp.lower not in ["a1","a2","a3","b1","b2","b3","c1","c2","c3"]:
                 print("Wrong input. Choose an existing cell.")
                 time.sleep(2)
+                continue
 
-        # change board column a
-        if inp == "a1":
-            board.cells[0][0] = self.sign
-        if inp == "a2":
-            board.cells[1][0] = self.sign
-        if inp == "a3":
-            board.cells[2][0] = self.sign
-        
-        # change board column b
-        if inp == "b1":
-            board.cells[0][1] = self.sign
-        if inp == "b2":
-            board.cells[1][1] = self.sign
-        if inp == "b3":
-            board.cells[2][1] = self.sign
+            # change board column a
+            if inp == "a1" and board_cells[0][0] != " ":
+                board.cells[0][0] = self.sign
+                break
+            if inp == "a2" and board_cells[1][0] != " ":
+                board.cells[1][0] = self.sign
+                break
+            if inp == "a3" and board_cells[2][0] != " ":
+                board.cells[2][0] = self.sign
+                break
+            
+            # change board column b
+            if inp == "b1" and board_cells[0][1] != " ":
+                board.cells[0][1] = self.sign
+                break
+            if inp == "b2" and board_cells[1][1] != " ":
+                board.cells[1][1] = self.sign
+                break
+            if inp == "b3" and board_cells[2][1] != " ":
+                board.cells[2][1] = self.sign
+                break
 
-        # change board column c
-        if inp == "c1":
-            board.cells[0][2] = self.sign
-        if inp == "c2":
-            board.cells[1][2] = self.sign
-        if inp == "c3":
-            board.cells[2][2] = self.sign
-
+            # change board column c
+            if inp == "c1" and board_cells[0][2] != " ":
+                board.cells[0][2] = self.sign
+                break
+            if inp == "c2" and board_cells[1][2] != " ":
+                board.cells[1][2] = self.sign
+                break
+            if inp == "c3" and board_cells[2][2] != " ":
+                board.cells[2][2] = self.sign
+                break
+            
+            else:
+                print("Cell already taken. Choose another one.")
+                inp = ""
 
 
 board_cells = []
@@ -93,4 +106,5 @@ player2 = Player(input("Type the name of Player 2: "), "O")
 
 board = Board(board_cells)
 
+player1.make_move(board)
 print(board)
