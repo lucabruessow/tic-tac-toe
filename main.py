8d2bf0c9aef8ccd677575e4a8853c2e20f0b2b34
Luca Brüssow <210828907+lucabruessow@users.noreply.github.com>
2025-06-05 13:51:12 +0200

    fix: correct winning conditions

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import time
+import os
+
+def clear_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
 
 class Board:
     def __init__(self, cells):
@@ -15,25 +19,25 @@ class Board:
     
     def game_finished(self):
         # all horizontal options
-        if self.cells[0][0] == self.cells[0][1] == self.cells[0][2] and not " ":
+        if self.cells[0][0] == self.cells[0][1] == self.cells[0][2] and self.cells[0][0] != " ":
             return True
-        if self.cells[1][0] == self.cells[1][1] == self.cells[1][2] and not " ":
+        if self.cells[1][0] == self.cells[1][1] == self.cells[1][2] and self.cells[1][0] != " ":
             return True
-        if self.cells[2][0] == self.cells[2][1] == self.cells[2][2] and not " ":
+        if self.cells[2][0] == self.cells[2][1] == self.cells[2][2] and self.cells[2][0] != " ":
             return True
         
         # all vertical options
-        if self.cells[0][0] == self.cells[1][0] == self.cells[2][0] and not " ":
+        if self.cells[0][0] == self.cells[1][0] == self.cells[2][0] and self.cells[0][0] != " ":
             return True
-        if self.cells[0][1] == self.cells[1][1] == self.cells[2][1] and not " ":
+        if self.cells[0][1] == self.cells[1][1] == self.cells[2][1] and self.cells[0][1] != " ":
             return True
-        if self.cells[0][2] == self.cells[1][2] == self.cells[2][2] and not " ":
+        if self.cells[0][2] == self.cells[1][2] == self.cells[2][2] and self.cells[0][2] != " ":
             return True
 
         # all diagonal options
-        if self.cells[0][0] == self.cells[1][1] == self.cells [2][2] and not " ":
+        if self.cells[0][0] == self.cells[1][1] == self.cells [2][2] and self.cells[0][0] != " ":
             return True
-        if self.cells[0][2] == self.cells[1][1] == self.cells [2][0] and not " ":
+        if self.cells[0][2] == self.cells[1][1] == self.cells [2][0] and self.cells[2][0] != " ":
             return True
 
         # draw
@@ -43,56 +47,88 @@ class Board:
         else:
             return False
 
+    def game_start(self, player1, player2):
+        while not self.game_finished():
+            clear_screen()
+            print(board)
+            
+            if player1.active:
+                player1.make_move(self, player2)
+
+            elif player2.active:
+                player2.make_move(self, player1)            
+            
+
 
 class Player:
-    def __init__(self, name, sign):
+    def __init__(self, name, sign, active):
         
         self.name = name
         self.sign = sign
+        self.active = active
 
     def __str__(self):
         return f"{self.name}"
     
-    def make_move(self, board):
+    def make_move(self, board, player):
+        print("test")
         inp = ""
         while inp.lower not in ["a1","a2","a3","b1","b2","b3","c1","c2","c3"]:
             inp = input("Choose a cell: ")
-            if inp.lower not in ["a1","a2","a3","b1","b2","b3","c1","c2","c3"]:
+            if inp.lower() not in ["a1","a2","a3","b1","b2","b3","c1","c2","c3"]:
                 print("Wrong input. Choose an existing cell.")
                 time.sleep(2)
                 continue
 
             # change board column a
-            if inp == "a1" and board_cells[0][0] != " ":
+            if inp == "a1" and board.cells[0][0] == " ":
                 board.cells[0][0] = self.sign
-                break
-            if inp == "a2" and board_cells[1][0] != " ":
+                self.active = False
+                player.active = True
+                break            
+            if inp == "a2" and board.cells[1][0] == " ":
                 board.cells[1][0] = self.sign
+                self.active = False
+                player.active = True
                 break
-            if inp == "a3" and board_cells[2][0] != " ":
+            if inp == "a3" and board.cells[2][0] == " ":
                 board.cells[2][0] = self.sign
+                self.active = False
+                player.active = True
                 break
             
             # change board column b
-            if inp == "b1" and board_cells[0][1] != " ":
+            if inp == "b1" and board.cells[0][1] == " ":
                 board.cells[0][1] = self.sign
+                self.active = False
+                player.active = True
                 break
-            if inp == "b2" and board_cells[1][1] != " ":
+            if inp == "b2" and board.cells[1][1] == " ":
                 board.cells[1][1] = self.sign
+                self.active = False
+                player.active = True
                 break
-            if inp == "b3" and board_cells[2][1] != " ":
+            if inp == "b3" and board.cells[2][1] == " ":
                 board.cells[2][1] = self.sign
+                self.active = False
+                player.active = True
                 break
 
             # change board column c
-            if inp == "c1" and board_cells[0][2] != " ":
+            if inp == "c1" and board.cells[0][2] == " ":
                 board.cells[0][2] = self.sign
+                self.active = False
+                player.active = True
                 break
-            if inp == "c2" and board_cells[1][2] != " ":
+            if inp == "c2" and board.cells[1][2] == " ":
                 board.cells[1][2] = self.sign
+                self.active = False
+                player.active = True
                 break
-            if inp == "c3" and board_cells[2][2] != " ":
+            if inp == "c3" and board.cells[2][2] == " ":
                 board.cells[2][2] = self.sign
+                self.active = False
+                player.active = True
                 break
             
             else:
@@ -100,11 +136,13 @@ class Player:
                 inp = ""
 
 
+
+
 board_cells = []
-player1 = Player(input("Type the name of player 1: "), "X")
-player2 = Player(input("Type the name of Player 2: "), "O")
+
+player1 = Player(input("Type the name of player 1: "), "X", True)
+player2 = Player(input("Type the name of Player 2: "), "O", False)
 
 board = Board(board_cells)
 
-player1.make_move(board)
-print(board)
+board.game_start(player1, player2)
